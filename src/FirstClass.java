8f12f4035555c3e3907ba048d3f291bd444b3bf0
Landon Davis <42148212+3D0X4@users.noreply.github.com>
2021-09-19 15:08:40 -0400

    first commit

---
+++ src/FirstClass.java
@@ -0,0 +1,21 @@
+
+import org.osbot.rs07.script.Script;
+
+import org.osbot.rs07.script.ScriptManifest;
+
+import java.awt.*;
+
+@ScriptManifest(name = "Skeleton", author = "Alek", version = 1.0, info = "", logo = "")
+
+public class FirstClass extends Script {
+
+    @Override
+
+    public int onLoop() {
+
+        return 100; //The amount of time in milliseconds before the loop starts over
+
+    }
+
+
+}
